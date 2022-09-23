bf15c1a5ff13488607266a50732d71233b7418d3
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-23 16:19:25 +0530

    Nav Graph

--- app/src/main/java/com/priyam/notesapp/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/priyam/notesapp/ui/login/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.priyam.notesapp.R
 
 class RegisterFragment : Fragment() {
@@ -15,7 +17,13 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        val txtRdirect = view.findViewById<TextView>(R.id.txtRedirect)
+        txtRdirect.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+        return view
     }
 
 }
