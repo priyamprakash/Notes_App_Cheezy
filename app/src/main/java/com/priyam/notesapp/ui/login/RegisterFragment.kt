0ff89190896c5d53a9d6623dacf3344bf62b0aea
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-25 08:11:04 +0530

    Minor Changes

--- app/src/main/java/com/priyam/notesapp/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/priyam/notesapp/ui/login/RegisterFragment.kt
@@ -8,22 +8,27 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.priyam.notesapp.R
+import com.priyam.notesapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
 
-        val txtRdirect = view.findViewById<TextView>(R.id.txtRedirect)
-        txtRdirect.setOnClickListener {
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        binding.txtError.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
-        return view
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
